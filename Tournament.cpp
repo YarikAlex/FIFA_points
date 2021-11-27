7d6ecb0bb9f150fa6dab30d2c28e01a90e09ec1a
Alexander <YaroslavtcevAV@gmail.com>
2021-11-27 15:27:25 +0300

    Added comments, exceptions. Tournament selection moved to main.cpp

--- Tournament.cpp
+++ Tournament.cpp
@@ -1,43 +1,45 @@
 #include "Tournament.h"
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 
-Tournament::Tournament(std::string euroPath, std::string euroGroupPath, std::string worldCupPath, std::string worldCupGroupPath)
+Tournament::Tournament(int choice, std::string& path, std::string& pathGroup)
 {
   const int EURO = 1;
   const int WORLD = 2;
-  switch (ChooseTournament())
+  if (choice == EURO)
   {
-  case EURO:
-	AddParticipants(euroPath, m_euroParticipants);
-	SetGroup(euroGroupPath, m_numberEuroGroup);
-	break;
-  case WORLD:
-	AddParticipants(worldCupPath, m_worldCupParticipants);
-	SetGroup(worldCupGroupPath, m_numberWorldCupGroup);
-	break;
-  default:
-	std::cout << "Default case!" << std::endl;
-	break;
+	try
+	{
+	  addParticipants(path, m_euroParticipants);
+	  setGroup(pathGroup, m_numberEuroGroup);
+	}
+	catch (const std::exception& ex)
+	{
+	  std::cout << "Error! "<< ex.what() << std::endl;
+	}
+	
+  }
+  if (choice == WORLD)
+  {
+	try
+	{
+	  addParticipants(path, m_worldCupParticipants);
+	  setGroup(pathGroup, m_numberWorldCupGroup);
+	}
+	catch (const std::exception& ex)
+	{
+	  std::cout << "Error!" << ex.what() << std::endl;
+	}
   }
 }
-int Tournament::ChooseTournament()
-{
-  int choice;
-  do {
-	std::cout << "---Select a tournament---" << std::endl;
-	std::cout << "Press 1 for Euro or 2 for World Cup!" << std::endl;
-	std::cin >> choice;
-  } while (choice < 1 or choice > 2);
-  return choice;
-}
 
-void Tournament::AddParticipants(std::string& path, int numberParticipants)
+void Tournament::addParticipants(std::string& path, int numberParticipants) //The function reads participants from a file.
 {
   std::ifstream participantsList;
   participantsList.open(path);
   if (!participantsList.is_open() and !participantsList.good())
-	std::cerr << "File is not open!" << std::endl;
+	throw std::exception("Unable to add participants. File not found");
   else
   {
 	std::string name;
@@ -46,39 +48,44 @@ void Tournament::AddParticipants(std::string& path, int numberParticipants)
 	for (int i = 0; i < numberParticipants; ++i)
 	{
 	  participantsList >> name >> rank >> fifa_points;
-	  std::shared_ptr<Team> p_new_team(new Team(name, rank, fifa_points));
-	  m_participants.push_back(p_new_team);
+	  auto newTeam = std::make_shared<Team>(name, rank, fifa_points);
+	  m_participants.push_back(newTeam);
 	}
   }
   participantsList.close();
 }
-void Tournament::SetGroup(std::string& path, const int tournamentGroups)
+void Tournament::setGroup(std::string& path, const int tournamentGroups) //The function distributes the participants into groups.
 {
   std::ifstream groupList;
   groupList.open(path);
   if(!groupList.is_open() and !groupList.good())
-	std::cerr << "File is not open!" << std::endl;
+	throw std::exception("Unable to set groups. File not found!");
   else
   {
-	std::string name;
 	for (int numberGroups = 0; numberGroups < tournamentGroups; ++numberGroups)
 	{
-	  std::shared_ptr<Group> p_new_group(new Group);
-	  for (int numberTeams = 0; numberTeams < p_new_group->numberTeams; ++numberTeams)
+	  auto newGroup = std::make_shared<Group>();
+	  newGroup->teams.resize(newGroup->numberTeams);
+	  std::generate(newGroup->teams.begin(), newGroup->teams.end(), [&]()
 	  {
+		std::string name;
 		groupList >> name;
-		for (auto it : m_participants)
-		{
-		  if (it->name == name)
-			p_new_group->teams.push_back(it);
-		}
-	  }
-	  m_groups.push_back(p_new_group);
+		return addTeam(name);
+	  });
+	  m_groups.push_back(newGroup);
 	}
   }
   groupList.close();
 }
-void Tournament::PlayGroupRound()
+std::shared_ptr<Team> Tournament::addTeam(std::string& name)
+{
+  for (auto it : m_participants)
+  {
+	if (it->name == name)
+	  return it;
+  }
+}
+void Tournament::playGroupRound()
 {
   int numberRounds = 3;
   for (int i = 0; i < numberRounds; ++i)
@@ -88,31 +95,31 @@ void Tournament::PlayGroupRound()
 	  int rank = 0;
 	  if (i == 0)
 	  {
-		std::cout << "----Round #1" << std::endl;
-		it->PlayMatch(it->teams[0], it->teams[1]);
-		it->PlayMatch(it->teams[2], it->teams[3]);
+		std::cout << "----Round #1----" << std::endl;
+		it->playMatch(it->teams[0], it->teams[1]);
+		it->playMatch(it->teams[2], it->teams[3]);
 		std::cout << it<<std::endl;
 	  }
 	  if (i == 1)
 	  {
-		std::cout << "----Round #2" << std::endl;
-		it->PlayMatch(it->teams[0], it->teams[2]);
-		it->PlayMatch(it->teams[1], it->teams[3]);
+		std::cout << "----Round #2----" << std::endl;
+		it->playMatch(it->teams[0], it->teams[2]);
+		it->playMatch(it->teams[1], it->teams[3]);
 		std::cout << it;
 	  }
 	  if (i == 2)
 	  {
-		std::cout << "----Round #3" << std::endl;
-		it->PlayMatch(it->teams[0], it->teams[3]);
-		it->PlayMatch(it->teams[1], it->teams[2]);
-		it->SortGroup();
+		std::cout << "----Round #3----" << std::endl;
+		it->playMatch(it->teams[0], it->teams[3]);
+		it->playMatch(it->teams[1], it->teams[2]);
+		it->sortGroup();
 		for_each(it->teams.begin(), it->teams.end(), [&](std::shared_ptr<Team>& team) {team->place = ++rank; });
 		std::cout << it;
 	  }
 	}
   }
 }
-void Tournament::SetEuroPlayOff()
+void Tournament::setEuroPlayOff()
 {
   //We split the first, second and third places into three baskets.
   std::queue<std::shared_ptr<Team>> firstTeams; //A1, B1, C1, D1, E1, F1
@@ -139,8 +146,21 @@ void Tournament::SetEuroPlayOff()
   //Remove fifth and sixth teams
   for (int i = 0; i < 2; ++i)
   {
-	preparationThirdTeams.pop_back();
-	preparationThirdTeams.shrink_to_fit();
+	try
+	{
+	  if(preparationThirdTeams.empty())
+		throw std::exception("Vector preparationThirdTeams is empty");
+	  else
+	  {
+		preparationThirdTeams.pop_back();
+		preparationThirdTeams.shrink_to_fit();
+	  }
+	}
+	catch (const std::exception& ex)
+	{
+	  std::cout << "Error! " << ex.what() << std::endl;
+	}
+	
   }
   //put commands in the queue
   for (auto it : preparationThirdTeams)
@@ -151,71 +171,92 @@ void Tournament::SetEuroPlayOff()
   //Create a playoff grid
   for (int i = 0; i < 2; ++i)
   {
-	//A2-B2 and D2-F2
-	std::shared_ptr<Team> firstTmpTeam = secondTeams.front();
-	secondTeams.pop();
-	std::shared_ptr<Team> secondTmpTeam = secondTeams.front();
-	secondTeams.pop();
-	if (i == 0)
-	  left_side.push_back(CreateMatch(firstTmpTeam, secondTmpTeam));
-	if (i == 1)
-	  right_side.push_back(CreateMatch(firstTmpTeam, secondTmpTeam));
+	try
+	{
+	  if (firstTeams.empty())
+		throw std::exception("Queue firstTeams is empty");
+	  if (secondTeams.empty())
+		throw std::exception("Queue secondTeams is empty");
+	  if (thirdTeams.empty())
+		throw std::exception("Queue thirdTeamss is empty");
+	  else
+	  {
+		//A2-B2 and D2-F2
+		std::shared_ptr<Team> firstTmpTeam = secondTeams.front();
+		secondTeams.pop();
+		std::shared_ptr<Team> secondTmpTeam = secondTeams.front();
+		secondTeams.pop();
+		if (i == 0)
+		  m_leftSide.push_back(createMatch(firstTmpTeam, secondTmpTeam));
+		if (i == 1)
+		  m_rightSide.push_back(createMatch(firstTmpTeam, secondTmpTeam));
 
-	//A1-C2 and D1-F2
-	firstTmpTeam = firstTeams.front();
-	firstTeams.pop();
-	secondTmpTeam = secondTeams.front();
-	secondTeams.pop();
-	if (i == 0)
-	  right_side.push_back(CreateMatch(firstTmpTeam, secondTmpTeam));
-	if (i == 1)
-	  left_side.push_back(CreateMatch(firstTmpTeam, secondTmpTeam));
+		//A1-C2 and D1-F2
+		firstTmpTeam = firstTeams.front();
+		firstTeams.pop();
+		secondTmpTeam = secondTeams.front();
+		secondTeams.pop();
+		if (i == 0)
+		  m_rightSide.push_back(createMatch(firstTmpTeam, secondTmpTeam));
+		if (i == 1)
+		  m_leftSide.push_back(createMatch(firstTmpTeam, secondTmpTeam));
 
-	//B1-third-2 and E1-third-4
-	std::shared_ptr<Team> thirdTmpTeam = thirdTeams.front(); //get third-1 and third-4 from the queue
-	thirdTeams.pop();
-	firstTmpTeam = firstTeams.front();
-	firstTeams.pop();
-	secondTmpTeam = thirdTeams.front();
-	thirdTeams.pop();
-	if (i == 0)
-	  right_side.push_back(CreateMatch(firstTmpTeam, secondTmpTeam));
-	if (i == 1)
-	  left_side.push_back(CreateMatch(firstTmpTeam, secondTmpTeam));
+		//B1-third-2 and E1-third-4
+		std::shared_ptr<Team> thirdTmpTeam = thirdTeams.front(); //get third-1 and third-4 from the queue
+		thirdTeams.pop();
+		firstTmpTeam = firstTeams.front();
+		firstTeams.pop();
+		secondTmpTeam = thirdTeams.front();
+		thirdTeams.pop();
+		if (i == 0)
+		  m_rightSide.push_back(createMatch(firstTmpTeam, secondTmpTeam));
+		if (i == 1)
+		  m_leftSide.push_back(createMatch(firstTmpTeam, secondTmpTeam));
 
-	//C1-third-1 and F1-third-3
-	firstTmpTeam = firstTeams.front();
-	firstTeams.pop();
-	if (i == 0)
-	  left_side.push_back(CreateMatch(firstTmpTeam, thirdTmpTeam));
-	if (i == 1)
-	  right_side.push_back(CreateMatch(firstTmpTeam, thirdTmpTeam));
+		//C1-third-1 and F1-third-3
+		firstTmpTeam = firstTeams.front();
+		firstTeams.pop();
+		if (i == 0)
+		  m_leftSide.push_back(createMatch(firstTmpTeam, thirdTmpTeam));
+		if (i == 1)
+		  m_rightSide.push_back(createMatch(firstTmpTeam, thirdTmpTeam));
+	  }
+	}
+	catch (const std::exception& ex)
+	{
+	  std::cout << "Error! " << ex.what() << std::endl;
+	}
   }
   int count = 0;
-  for (auto it : left_side)
+  for (auto it : m_leftSide)
   {
 	std::cout << "Match #" << ++count << std::endl;
 	std::cout << it.first_team << std::endl << it.second_team << std::endl << std::endl;
   }
-  for (auto it : right_side)
+  for (auto it : m_rightSide)
   {
 	std::cout << "Match #" << ++count << std::endl;
 	std::cout << it.first_team << std::endl << it.second_team << std::endl << std::endl;
   }
 }
-void Tournament::PrintParticipants()
+void Tournament::printParticipants()
 {
-  for (auto it : m_participants)
-	std::cout <<std::setw(10)<< it->name << "\t" << it->fifaRank << "\t" << it->fifaPoints << std::endl;
+  sort(m_participants.begin(), m_participants.end(), [](std::shared_ptr<Team> lhs, std::shared_ptr<Team> rhs)
+	{
+	  return lhs->fifaPoints > rhs->fifaPoints;
+	});
+  for (auto itTeam : m_participants)
+	std::cout << std::setw(10) << itTeam->name << "\t" << itTeam->startingFifaPoints << "\t" << itTeam->fifaPoints << "\t"
+	<< itTeam->fifaPoints - itTeam->startingFifaPoints << std::endl;
 }
 
-void Tournament::PrintGroup()
+void Tournament::printGroup()
 {
   for (auto it : m_groups)
 	std::cout << it << std::endl;
 }
 
-Match Tournament::CreateMatch(std::shared_ptr<Team> lhs, std::shared_ptr<Team> rhs)
+Match Tournament::createMatch(std::shared_ptr<Team> lhs, std::shared_ptr<Team> rhs) const
 {
   Match new_match(lhs, rhs);
   return new_match;
--- Tournament.h
+++ Tournament.h
@@ -8,23 +8,22 @@ class Tournament
 {
 private:
   std::vector<std::shared_ptr<Group>> m_groups;
-  std::vector<Match> left_side;
-  std::vector<Match> right_side;
+  std::vector<Match> m_leftSide;
+  std::vector<Match> m_rightSide;
   std::vector<std::shared_ptr<Team>> m_participants;
   static constexpr int m_euroParticipants = 24;
   static constexpr int m_numberEuroGroup = 6;
   static constexpr int m_worldCupParticipants = 32;
   static constexpr int m_numberWorldCupGroup = 8;
-  Match CreateMatch(std::shared_ptr<Team> lhs, std::shared_ptr<Team> rhs);
+  Match createMatch(std::shared_ptr<Team> lhs, std::shared_ptr<Team> rhs) const;
+  void addParticipants(std::string& path, int numberParticipants);
+  void setGroup(std::string& path, const int tournamentGroups);
+  std::shared_ptr<Team> addTeam(std::string& name);
 public:
-  Tournament(std::string euroPath, std::string euroGroupPath, std::string worldCupPath, std::string worldCupGroupPath);
+  Tournament(int choice, std::string& path, std::string& pathGroup);
   ~Tournament() = default;
-  int ChooseTournament();
-  void AddParticipants(std::string& path, int numberParticipants);
-  void SetGroup(std::string& path, const int tournamentGroups);
-  void PlayGroupRound();
-  void SetEuroPlayOff();
-  void PrintParticipants();
-  void PrintGroup();
-
+  void playGroupRound();
+  void setEuroPlayOff();
+  void printParticipants();
+  void printGroup();
 };
