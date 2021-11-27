d1cc57943702d3af3730a736bfdb29134a37362a
Alexander <YaroslavtcevAV@gmail.com>
2021-11-27 15:28:17 +0300

    Added tournament selection

--- main.cpp
+++ main.cpp
@@ -13,11 +13,32 @@ int main()
   const string euroGroupPath = "../FIFApoints/EuroCupGroups.txt";
   const string worldCupPath = "../FIFApoints/WorldCupParticipants.txt";
   const string worldCupGroupPath = "../FIFApoints/WorldCupGroups.txt";
-  Tournament tournament(euroPath, euroGroupPath, worldCupPath, worldCupGroupPath);
-  tournament.PrintParticipants();
-  tournament.PrintGroup();
-  tournament.PlayGroupRound();
-  tournament.SetEuroPlayOff();
+  const int EURO = 1;
+  const int WORLD = 2;
+
+  int choice;
+  do {
+	std::cout << "---Select a tournament---" << std::endl;
+	std::cout << "Press 1 for Euro or 2 for World Cup!" << std::endl;
+	std::cin >> choice;
+  } while (choice < 1 or choice > 2);
+
+  string pathParticipants, pathGroup;
+  if (choice == EURO)
+  {
+	pathParticipants = euroPath;
+	pathGroup = euroGroupPath;
+  }
+  if (choice == WORLD)
+  {
+	pathParticipants = worldCupPath;
+	pathGroup = worldCupGroupPath;
+  }
+  Tournament tournament(choice, pathParticipants, pathGroup);
+  tournament.printParticipants();
+  tournament.printGroup();
+  tournament.playGroupRound();
+  tournament.setEuroPlayOff();
 
   return 0;
 }
