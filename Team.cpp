3d344a7ad41e4b29268f7b2e5d4661dbfa96e806
Alexander <YaroslavtcevAV@gmail.com>
2021-11-27 15:15:24 +0300

    The evaluatingStats function has been changed. Now it does not change the data in the object, but returns it.

--- Team.cpp
+++ Team.cpp
@@ -9,28 +9,32 @@ Team::Team(std::string nameNew, int fifaRangNew, double fifaPointsNew) :
   goalsScored = 0;
   goalsConceded = 0;
   goalsDifference = 0;
+  startingFifaPoints = fifaPoints;
 }
 
-void Team::CountingStats(int scored, int conceded, double importanceOfMatch, const double anotherTeamPoints)
+const std::tuple<int, int, int, double> Team::evaluatingStats(int scored, int conceded, double importanceOfMatch, const double anotherTeamPoints)
 {
-  goalsScored += scored;
-  goalsConceded += conceded;
-  goalsDifference = goalsScored - goalsConceded;
   games++;
   fifaBefore = fifaPoints;
-  double difference = this->fifaPoints - anotherTeamPoints;
-  double ExpectedResultOfTheMatch = 1 / (pow(10, -difference / 600) + 1);
-  //New_fifa_points = Fifa_points + ImportanceOfMatches * (ResultOfMatch - ExpectedResultOfTheMatch)
+  /*
+  New_fifa_points = Fifa_points + ImportanceOfMatches * (ResultOfMatch - ExpectedResultOfTheMatch)
+  ExpectedResultOfTheMatch = 1/(10^-(FIFA points of this team - FIFA points of another team)/600) + 1
+  ResultOfMatch = 1 if this team win, 0.5 if draw and 0 if lose
+  */
+  double ExpectedResultOfTheMatch = 1 / (pow(10, -(this->fifaPoints - anotherTeamPoints) / 600) + 1);
   if (scored > conceded) {
 	points += 3;
-	fifaPoints = fifaPoints + importanceOfMatch * (1 - ExpectedResultOfTheMatch);
+	return std::make_tuple(goalsScored + scored, goalsConceded + conceded, scored - conceded,
+	  fifaPoints + importanceOfMatch * (1 - ExpectedResultOfTheMatch));
   }
   if (scored == conceded) {
 	points++;
-	fifaPoints = fifaPoints + importanceOfMatch * (0.5 - ExpectedResultOfTheMatch);
+	return std::make_tuple(goalsScored + scored, goalsConceded + conceded, scored - conceded,
+	  fifaPoints + importanceOfMatch * (0.5 - ExpectedResultOfTheMatch));
   }
-  if(scored<conceded)
-	fifaPoints = fifaPoints + importanceOfMatch * (0 - ExpectedResultOfTheMatch);
+  if (scored < conceded)
+	return std::make_tuple(goalsScored + scored, goalsConceded + conceded, scored - conceded,
+	  fifaPoints + importanceOfMatch * (0 - ExpectedResultOfTheMatch));
 }
 
 std::ostream& operator<< (std::ostream& out, const std::shared_ptr<Team>& team)
--- Team.h
+++ Team.h
@@ -7,12 +7,11 @@ class Team
 {
 public:
   std::string name;
-  double fifaPoints, fifaBefore;
+  double fifaPoints, fifaBefore, startingFifaPoints;
   int fifaRank, place, games, points, goalsScored, goalsConceded, goalsDifference;
   Team(std::string nameNew, int fifaRankNew, double fifaPointsNew);
   ~Team() = default;
-  void CountingStats(int scored, int conceded, double importanceOfMatch, double anotherTeamPoints);
-  //friend std::ostream& operator<< (std::ostream& out, const Team& team);
+  const std::tuple<int, int, int, double> evaluatingStats(int scored, int conceded, double importanceOfMatch, double anotherTeamPoints);
 };
 
 std::ostream& operator<< (std::ostream& out, const std::shared_ptr<Team>& team);
